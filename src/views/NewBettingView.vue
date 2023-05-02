1be27c4ae243ccbedf064d082e079f749f08f04f
MiskiPatrik <miski.patrik@students.jedlik.eu>
2023-05-02 17:27:04 +0200

    Bug fix

--- src/views/NewBettingView.vue
+++ src/views/NewBettingView.vue
@@ -1,10 +1,46 @@
+<script setup lang="ts">
+  import axios from "axios";
+  import { Notify } from "quasar";
+  import { onMounted } from "vue";
+  import { useTicketsStore } from "../store/ticketsStore";
+  const ticketsStore = useTicketsStore();
+  Notify.setDefaults({
+    position: "bottom",
+    textColor: "white",
+    timeout: 3000,
+    actions: [{ icon: "close", color: "white" }],
+    progress: true,
+  });
+  onMounted(() => {
+    axios
+      .get("http://localhost:8000/api/bets")
+      .then((response) => {
+        ticketsStore.data = response.data;
+        const uniqueValues = new Set();
+        ticketsStore.data.forEach((item) => {
+          uniqueValues.add(item.category);
+        });
+        ticketsStore.fieldNames = Array.from(uniqueValues) as string[];
+        ticketsStore.filteredData = [];
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    ticketsStore.showTable = false;
+  });
+</script>
 <template>
   <div>
-    <q-btn color="primary" v-for="(category, index) in fieldNames" :key="index" @click="filterData(category)">
-      {{ category }}
+    <q-btn
+      color="primary"
+      v-for="(categoryName, index) in ticketsStore.fieldNames"
+      :key="index"
+      @click="ticketsStore.filterData(categoryName)"
+    >
+      {{ categoryName }}
     </q-btn>
 
-    <table v-if="showTable">
+    <table v-if="ticketsStore.showTable">
       <thead>
         <tr>
           <th>Title</th>
@@ -14,143 +50,39 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-if="filteredData.length === 0">
+        <tr v-if="ticketsStore.filteredData.length === 0">
           <td colspan="2">No data found</td>
         </tr>
-        <tr v-for="(item, index) in filteredData" :key="index">
+        <tr v-for="(item, index) in ticketsStore.filteredData" :key="index">
           <td>{{ item.title }}</td>
           <td>{{ item.date }}</td>
           <td>{{ item.odds }}</td>
           <td v-if="item.category == 'versus'">{{ item.odds2 }}</td>
-          <td><q-btn color="primary" @click="openBetDialog(item)">Bet</q-btn></td>
+          <td><q-btn color="primary" @click="ticketsStore.openBetDialog(item)">Bet</q-btn></td>
         </tr>
       </tbody>
     </table>
-    <q-dialog v-model="showBetDialog" persistent>
+    <q-dialog v-model="ticketsStore.showBetDialog" persistent>
       <q-card>
         <q-card-section>
           <div>
             Bet amount:
-            <div v-if="selectedItem.category == 'versus'" class="q-gutter-md">
-              <q-radio v-model="picked" val="first" label="Driver 1" />
-              <q-radio v-model="picked" val="second" label="Driver 2" />
+            <div v-if="ticketsStore.selectedItem.category == 'versus'" class="q-gutter-md">
+              <q-radio v-model="ticketsStore.picked" val="first" label="Driver 1" />
+              <q-radio v-model="ticketsStore.picked" val="second" label="Driver 2" />
             </div>
-            <q-input v-model="ticketBetAmount" />
+            <q-input v-model="ticketsStore.ticketBetAmount" />
           </div>
         </q-card-section>
         <q-card-actions align="right">
-          <q-btn label="Cancel" @click="closeBetDialog()" />
-          <q-btn color="primary" label="Confirm" @click="sendTicket()" />
+          <q-btn label="Cancel" @click="ticketsStore.closeBetDialog()" />
+          <q-btn color="primary" label="Confirm" @click="ticketsStore.sendTicket()" />
         </q-card-actions>
       </q-card>
     </q-dialog>
   </div>
 </template>
 
-<script>
-  import axios from "axios";
-  import { Notify } from "quasar";
-
-  Notify.setDefaults({
-    position: "bottom",
-    textColor: "white",
-    timeout: 3000,
-    actions: [{ icon: "close", color: "white" }],
-    progress: true,
-  });
-  export default {
-    data() {
-      return {
-        data: [],
-        filteredData: [],
-        filterValue: "",
-        fieldNames: [],
-        showTable: false,
-        showBetDialog: false,
-        ticketBetAmount: null,
-        userId: null,
-        picked: null,
-      };
-    },
-    created() {
-      axios
-        .get("http://localhost:8000/api/bets")
-        .then((response) => {
-          this.data = response.data;
-          const uniqueValues = new Set();
-          this.data.forEach((item) => {
-            uniqueValues.add(item.category);
-          });
-          this.fieldNames = Array.from(uniqueValues);
-          this.filteredData = [];
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    methods: {
-      filterData(category) {
-        this.filteredData = this.data.filter((item) => item.category === category);
-        this.showTable = true;
-      },
-      openBetDialog(item) {
-        this.showBetDialog = true;
-        this.selectedItem = item;
-      },
-      closeBetDialog() {
-        this.showBetDialog = false;
-        this.selectedItem = null;
-        this.ticketBetAmount = null;
-        this.picked = null;
-      },
-      sendTicket() {
-        let sendOdds = this.selectedItem.odds;
-        if (this.selectedItem.category == "versus") {
-          if (this.picked == "first") {
-            sendOdds = this.selectedItem.odds;
-          } else {
-            sendOdds = this.selectedItem.odds2;
-          }
-        }
-        const today = new Date();
-        const currentDate = today.toISOString().slice(0, 10);
-        axios
-          .post(`http://localhost:8000/api/tickets`, {
-            status: "ongoing",
-            debt: this.ticketBetAmount,
-            sum_odds: sendOdds,
-            races: this.selectedItem.category,
-            payment_date: currentDate,
-            userID: localStorage.getItem("id"),
-            betID: this.selectedItem.id,
-          })
-          .then((response) => {
-            Notify.create({
-              message: `Bet with id=${response.data.ticketID} has been created successfully!`,
-              color: "positive",
-            });
-            this.closeBetDialog();
-          })
-          .catch((error) => {
-            if (error.response.data.message.includes("Insufficient balance")) {
-              Notify.create({
-                message: `Insufficient balance`,
-                color: "negative",
-              });
-            } else {
-              Notify.create({
-                message: `Error in create bet: ${error.response.data.message}`,
-                color: "negative",
-              });
-            }
-          })
-          .finally(() => {
-            this.closeBetDialog();
-          });
-      },
-    },
-  };
-</script>
 <style>
   table {
     border-collapse: collapse;
